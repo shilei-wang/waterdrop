a283226feae92d3f57d6be1eb057ab7ce7af2e24
shilei-wang <shelwin_wang@hotmail.com>
2021-01-14 17:29:10 +0800

    0

--- common/iris_home/0-5_genesis.sh
+++ common/iris_home/0-5_genesis.sh
@@ -8,6 +8,8 @@ if [ $1 == "irishub" ]
   sed -i '' 's/stake/uiris/g' /Users/sherlock/.iris/config/genesis.json
   sed -i '' 's/"supers": \[\]/"supers": \[{"description":"test","account_type":0,"address":"iaa1mufrktu9tc45pencvrxwwe0pwf3kvphrfdmt80","added_by":"iaa1mufrktu9tc45pencvrxwwe0pwf3kvphrfdmt80"}\]/g' /Users/sherlock/.iris/config/genesis.json
 
+  sed -i '' 's/swagger = false/swagger = true/g'  /Users/sherlock/.iris/config/app.toml
+  sed -i '' 's/enable = false/enable = true/g'  /Users/sherlock/.iris/config/app.toml
 
 #irishub自动化测试
   elif [ $1 == "irishub_autotest" ]
