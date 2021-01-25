8638ed14e1f3d67d5d704a8dfde95b70028088de
shilei-wang <shelwin_wang@hotmail.com>
2021-01-25 15:30:10 +0800

    0

--- common/iris_home/0-5_genesis.sh
+++ common/iris_home/0-5_genesis.sh
@@ -6,7 +6,13 @@ if [ $1 == "irishub" ]
   echo "Init irishub genesis.json"
 
   sed -i '' 's/stake/uiris/g' /Users/sherlock/.iris/config/genesis.json
-  sed -i '' 's/"supers": \[\]/"supers": \[{"description":"test","account_type":0,"address":"iaa1220rc0zqzn6h6xku40cvp8vndw3ywfw02dtc07","added_by":"iaa1220rc0zqzn6h6xku40cvp8vndw3ywfw02dtc07"}\]/g' /Users/sherlock/.iris/config/genesis.json
+  sed -i '' 's/"supers": \[\]/"supers": \[{"description":"test","account_type":0,"address":"iaa1x8nwtz579zth0dzghkrazpeyp0ua8hqu332xm0","added_by":"iaa1x8nwtz579zth0dzghkrazpeyp0ua8hqu332xm0"}\]/g' /Users/sherlock/.iris/config/genesis.json
+  sed -i '' 's/\"voting_period\": \"172800s\"/\"voting_period\": \"90s\"/g' /Users/sherlock/.iris/config/genesis.json
+
+#slash相关
+  sed -i '' 's/"signed_blocks_window": "100"/"signed_blocks_window": "20"/g' /Users/sherlock/.iris/config/genesis.json
+  sed -i '' 's/"downtime_jail_duration": "600s"/"downtime_jail_duration": "5s"/g' /Users/sherlock/.iris/config/genesis.json
+
 
   sed -i '' 's/swagger = false/swagger = true/g'  /Users/sherlock/.iris/config/app.toml
   sed -i '' 's/enable = false/enable = true/g'  /Users/sherlock/.iris/config/app.toml
@@ -15,11 +21,6 @@ if [ $1 == "irishub" ]
   elif [ $1 == "irishub_autotest" ]
   then
   echo "Init irishub_autotest genesis.json"
-
-  sed -i '' 's/stake/uiris/g' /Users/sherlock/.iris/config/genesis.json
-  sed -i '' 's/"supers": \[\]/"supers": \[{"description":"test","account_type":0,"address":"iaa1220rc0zqzn6h6xku40cvp8vndw3ywfw02dtc07","added_by":"iaa1220rc0zqzn6h6xku40cvp8vndw3ywfw02dtc07"}\]/g' /Users/sherlock/.iris/config/genesis.json
-
-
 
 fi
 
